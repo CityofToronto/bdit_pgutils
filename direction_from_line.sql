9b99635080377aa2b8fe1b674fc5e22fccf9becc
Raphael Dumas <raphael.a.dumas@gmail.com>
2016-11-22 17:54:41 -0500

    Creating direction from line function

---
+++ direction_from_line.sql
@@ -0,0 +1,13 @@
+﻿CREATE OR REPLACE FUNCTION direction_from_line(line GEOMETRY)
+RETURNS varchar(10)
+AS $$
+DECLARE
+	dir varchar(10);
+BEGIN 
+	SELECT direction INTO dir
+	FROM (VALUES ('Northbound', int4range(0,45)), ('Northbound', int4range(315,361)), ('Eastbound', int4range(45,135)), ('Southbound', int4range(135,225)), ('Westbound', int4range(225,315))) AS t(direction, angle_range)
+	WHERE degrees(ST_Azimuth(ST_StartPoint(ST_LineMerge(line)), ST_EndPoint(ST_LineMerge(line))))::INT <@ angle_range
+	LIMIT 1; 
+	RETURN dir;
+END;
+$$ LANGUAGE plpgsql
