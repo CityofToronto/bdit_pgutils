d76a19eefd816c3e3d3e665e6401ea8d7cf339dc
Raphael Dumas <raphael.a.dumas@gmail.com>
2017-03-16 16:45:48 -0400

    Two character direction function

---
+++ gis/twochar_direction.sql
@@ -0,0 +1,15 @@
+﻿CREATE OR REPLACE FUNCTION gis.twochar_direction(direction text)
+  RETURNS TEXT AS
+$BODY$
+BEGIN
+    RETURN CASE direction WHEN 'Northbound' THEN 'NB'
+			  WHEN 'Southbound' THEN 'SB'
+			  WHEN 'Eastbound' THEN 'EB'
+			  WHEN 'Westbound' THEN 'WB'
+	END;
+END;
+$BODY$
+  LANGUAGE plpgsql;
+ALTER FUNCTION gis.twochar_direction(text)
+  OWNER TO rdumas;
+GRANT EXECUTE ON FUNCTION gis.twochar_direction(text) TO public;
