4b2e0350b859727f15ef25fd39c3a09cf21858ae
Raphael Dumas <raphael.a.dumas@gmail.com>
2017-04-19 16:07:43 -0400

    Updated direction from line
    Adding exception.

--- gis/direction_from_line.sql
+++ gis/direction_from_line.sql
@@ -1,13 +1,19 @@
-﻿CREATE OR REPLACE FUNCTION direction_from_line(line GEOMETRY)
+﻿CREATE OR REPLACE FUNCTION gis.direction_from_line(line GEOMETRY)
 RETURNS varchar(10)
 AS $$
-DECLARE
-	dir varchar(10);
+DECLARE	dir varchar(10);
 BEGIN 
+
+	IF GeometryType(line) NOT IN ('MULTILINESTRING','LINESTRING') THEN
+		RAISE EXCEPTION 'Invalid Geometry'
+			USING HINT = 'Input geometry must be a Line';
+	END IF;
+	
 	SELECT direction INTO dir
 	FROM (VALUES ('Northbound', int4range(0,45)), ('Northbound', int4range(315,361)), ('Eastbound', int4range(45,135)), ('Southbound', int4range(135,225)), ('Westbound', int4range(225,315))) AS t(direction, angle_range)
 	WHERE degrees(ST_Azimuth(ST_StartPoint(ST_LineMerge(line)), ST_EndPoint(ST_LineMerge(line))))::INT <@ angle_range
 	LIMIT 1; 
 	RETURN dir;
 END;
-$$ LANGUAGE plpgsql
+$$ LANGUAGE 'plpgsql' IMMUTABLE STRICT 
+;
