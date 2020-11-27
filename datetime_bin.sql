d56fdfe469839beb34570b45f2a4c42aadd0624a
Raphael Dumas <radumas@users.noreply.github.com>
2020-11-27 09:46:49 -0500

    Moar bug

--- datetime_bin.sql
+++ datetime_bin.sql
@@ -12,5 +12,5 @@ AS $BODY$
 
 $BODY$;
 
-ALTER FUNCTION public.datetime_bin(timestamp without time zone)
+ALTER FUNCTION public.datetime_bin(timestamp without time zone, integer)
     OWNER TO rdumas;
