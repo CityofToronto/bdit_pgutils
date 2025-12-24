b2f58bf5c86cb34e0375480e70bf1ab594fbad9f
gabrielwol <80077912+gabrielwol@users.noreply.github.com>
2025-12-24 15:59:52 +0000

    #19 allow get_recursive_dependencies to recurse up or down

--- mermaid_diagrams/get_recursive_dependencies.sql
+++ mermaid_diagrams/get_recursive_dependencies.sql
@@ -1,49 +1,115 @@
+--DROP FUNCTION IF EXISTS public.get_recursive_dependencies;
+
 CREATE OR REPLACE FUNCTION public.get_recursive_dependencies (
-    sch_name text,
-    obj_name text
-) RETURNS TABLE(oid oid, obj_schema character varying, obj_name character varying) 
-    LANGUAGE sql
+    input_obj text,
+    recursive_direction text
+) RETURNS TABLE(oid oid, obj_schema name, obj_name name) 
+    LANGUAGE plpgsql
     COST 100
     VOLATILE PARALLEL UNSAFE
     ROWS 1000
 AS $BODY$
 
-    SELECT oid, obj_schema, obj_name
-        FROM (
-            WITH RECURSIVE recursive_deps(oid, obj_schema, obj_name, depth) AS
-            (
-                SELECT
-                    0::oid,
-                    get_recursive_dependencies.sch_name::character varying COLLATE "C",
-                    get_recursive_dependencies.obj_name::character varying COLLATE "C",
-                    0
-                UNION
-                SELECT
-                    deps.dep_oid,
-                    deps.dep_schema::varchar,
-                    deps.dep_name::varchar,
-                    recursive_deps.depth + 1
-                FROM
-                    (
-                        SELECT dep_oid, ref_schema, ref_name, dep_schema, dep_name
-                        FROM public.dependent_relations
-                    ) AS deps
-                JOIN recursive_deps ON
-                    deps.ref_schema = recursive_deps.obj_schema
-                    AND deps.ref_name = recursive_deps.obj_name
-                WHERE
-                    depth < 20
-                    AND NOT (
-                        deps.ref_schema = deps.dep_schema
-                        AND deps.ref_name = deps.dep_name
-                    )
-            )
+BEGIN
 
-            SELECT oid, obj_schema, obj_name, depth
-            FROM recursive_deps
-            WHERE depth >= 0
-        ) AS t
+    IF recursive_direction = 'down' THEN 
+        RETURN QUERY
+        SELECT t.oid, t.obj_schema, t.obj_name
+            FROM (
+                WITH RECURSIVE recursive_deps(oid, obj_schema, obj_name, depth) AS
+                (
+                    SELECT
+                        pg_class.oid,
+                        pg_namespace.nspname,
+                        pg_class.relname,
+                        0 AS depth
+                    FROM pg_class
+                    JOIN pg_namespace ON pg_class.relnamespace = pg_namespace.oid
+                    WHERE
+                        pg_namespace.nspname = split_part(input_obj, '.', 1)
+                        AND pg_class.relname = split_part(input_obj, '.', 2)
+    
+                    UNION
+                    SELECT
+                        deps.dep_oid,
+                        deps.dep_schema::varchar,
+                        deps.dep_name::varchar,
+                        recursive_deps.depth + 1
+                    FROM
+                        (
+                            SELECT dep_oid, ref_schema, ref_name, dep_schema, dep_name
+                            FROM public.dependent_relations
+                        ) AS deps
+                    JOIN recursive_deps ON
+                        deps.ref_schema = recursive_deps.obj_schema
+                        AND deps.ref_name = recursive_deps.obj_name
+                    WHERE
+                        depth < 20
+                        AND NOT (
+                            deps.ref_schema = deps.dep_schema
+                            AND deps.ref_name = deps.dep_name
+                        )
+                )
+    
+                SELECT rd.oid, rd.obj_schema, rd.obj_name, rd.depth
+                FROM recursive_deps AS rd
+                WHERE depth >= 0
+            ) AS t
         GROUP BY t.oid, t.obj_schema, t.obj_name
-        ORDER BY max(depth) DESC
+        ORDER BY max(depth) DESC;
+    ELSEIF recursive_direction = 'up' THEN
+        RETURN QUERY
+        SELECT t.oid, t.obj_schema, t.obj_name
+            FROM (
+                WITH RECURSIVE recursive_deps(oid, obj_schema, obj_name, depth) AS
+                (
+                    SELECT
+                        pg_class.oid,
+                        pg_namespace.nspname,
+                        pg_class.relname,
+                        0 AS depth
+                    FROM pg_class
+                    JOIN pg_namespace ON pg_class.relnamespace = pg_namespace.oid
+                    WHERE
+                        pg_namespace.nspname = split_part(input_obj, '.', 1)
+                        AND pg_class.relname = split_part(input_obj, '.', 2)
+    
+                    UNION
+                    SELECT
+                        deps.dep_oid,
+                        deps.ref_schema::varchar,
+                        deps.ref_name::varchar,
+                        recursive_deps.depth + 1
+                    FROM
+                        (
+                            SELECT dep_oid, ref_schema, ref_name, dep_schema, dep_name
+                            FROM public.dependent_relations
+                        ) AS deps
+                    JOIN recursive_deps ON
+                        deps.dep_schema = recursive_deps.obj_schema
+                        AND deps.dep_name = recursive_deps.obj_name
+                    WHERE
+                        depth < 20
+                        AND NOT (
+                            deps.ref_schema = deps.dep_schema
+                            AND deps.ref_name = deps.dep_name
+                        )
+                )
+    
+                SELECT rd.oid, rd.obj_schema, rd.obj_name, rd.depth
+                FROM recursive_deps AS rd
+                WHERE depth >= 0
+            ) AS t
+        GROUP BY t.oid, t.obj_schema, t.obj_name
+        ORDER BY max(depth) DESC;
+
+    END IF;
+
+    END;
 
 $BODY$;
+
+/*Examples:
+SELECT public.get_recursive_dependencies('miovision_validation.valid_legs_view', 'down')
+SELECT public.get_recursive_dependencies('miovision_validation.valid_legs_view', 'up')
+*/
