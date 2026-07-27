6e604f4ee92062beb37781677c595ab76ef569c0
Gabe Wolofsky <80077912+gabrielwol@users.noreply.github.com>
2026-07-27 13:43:02 -0400

    Create array_intersect function and aggregate

---
+++ create-function-array_intersect.sql
@@ -0,0 +1,30 @@
+CREATE OR REPLACE FUNCTION public.array_intersect(state int[], elem int[])
+RETURNS int[] AS
+$BODY$
+BEGIN
+    -- first row: seed the accumulator
+    IF state IS NULL THEN
+        RETURN elem;
+    END IF;
+
+    -- short-circuit: once we hit empty, stays empty
+    IF state = '{}' THEN
+        RETURN state;
+    END IF;
+
+    RETURN ARRAY(SELECT unnest(state) INTERSECT SELECT unnest(elem));
+END;
+$BODY$
+LANGUAGE plpgsql IMMUTABLE;
+
+CREATE AGGREGATE public.array_intersect_agg(int[]) (
+    SFUNC = array_intersect,
+    STYPE = int[]
+);
+
+--example of function
+SELECT public.array_intersect('{1,2}'::int[], '{2,3}'::int[])
+
+--example of aggregate function
+WITH test(vals) AS ((VALUES('{1,2}'::int[]), ('{2,3}'::int[]), ('{2,4}'::int[])))
+SELECT public.array_intersect_agg(vals) FROM test
